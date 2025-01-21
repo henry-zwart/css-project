e39648a6c3d117a4230c7aacb20503dbf36faf14
lisa1912 <lpijpers03@hotmail.com>
2025-01-21 15:57:35 +0100

    first implementation game of life and test

---
+++ scripts/test_simple_ca.py
@@ -0,0 +1,6 @@
+from css_project.simple_ca import GameOfLife
+a = GameOfLife(10)
+a.initial_grid(0.1)
+print(a.grid)
+
+a.update()
--- src/css_project/simple_ca.py
+++ src/css_project/simple_ca.py
@@ -1,1 +1,31 @@
-a=2
+import numpy as np
+
+class GameOfLife:
+    grid: np.ndarray
+    
+    def __init__(self, width: int = 128):
+        self.grid = np.zeros((width, width), dtype=int)
+        self.width = width
+
+    def initial_grid(self, p):
+        random_matrix = np.random.random(self.grid.shape)
+        self.grid = np.where(random_matrix < p, 1, 0)
+
+
+
+    def find_neighbors(self, x, y):
+        indexes = []
+        for delta_x in range(-1, 2):
+            if x+delta_x < 0 or x+delta_x > self.width - 1:
+                    continue
+            for delta_y in range(-1, 2):
+                if y+delta_y < 0 or y+delta_y > self.width - 1:
+                    continue
+                if delta_x==0 and delta_y==0:
+                    continue
+                indexes.append([x+delta_x, y+delta_y])
+        return indexes
+
+    def update(self):
+        test = self.find_neighbors(9, 0)
+        print(test)
