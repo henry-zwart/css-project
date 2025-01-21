b1b9ba26a38af8db0f49a711df449cab90f52b9c
fredloth <116084074+fredloth@users.noreply.github.com>
2025-01-21 17:45:38 +0100

    start on plotting ratio of alive cells

--- scripts/visualise.py
+++ scripts/visualise.py
@@ -2,7 +2,23 @@ from css_project.simple_ca import GameOfLife
 from css_project.visualisation import plot_grid
 
 if __name__ == "__main__":
-    game_of_life = GameOfLife(64)
+    width = 64
+
+    game_of_life = GameOfLife(width)
     game_of_life.initial_grid(p=0.5)
     fig, ax = plot_grid(game_of_life)
     fig.savefig("test_grid.png", dpi=300)
+
+    t = 0
+    total_cells = width * width
+    alive = [game_of_life.alive()]
+
+    while t < 10:
+        game_of_life.update()
+        alive.append(game_of_life.alive())
+        t += 1
+
+    fig, ax = plot_grid(game_of_life)
+    fig.savefig("test_grid_after.png", dpi=300)
+
+    # Plot ratio of alive vs dead cells
