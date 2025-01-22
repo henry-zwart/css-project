51cab7e8165a07e8b59941f66ad683b3b28d6fab
lisa1912 <lpijpers03@hotmail.com>
2025-01-22 14:09:31 +0100

    visualise_vegetation changed

--- scripts/visualise_vegetation.py
+++ scripts/visualise_vegetation.py
@@ -1,29 +1,41 @@
 from css_project.vegetation import Vegetation
-from css_project.visualisation import plot_grid
+from css_project.visualisation import animate_ca, plot_grid
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     width = 64
 
     vegetation = Vegetation(width)
     vegetation.initial_grid(p=0.5)
+
+    initial_grid = vegetation.grid.copy()
+
+    ani = animate_ca(vegetation, 10)
+    ani.save("vegetation.gif")
+
+    vegetation.grid = initial_grid.copy()
+
     fig, ax = plot_grid(vegetation)
     fig.savefig("veg_grid.png", dpi=300)
 
     t = 0
     total_cells = width * width
-    alive = [vegetation.total_alive()]
+    alive = [vegetation.total_alive() / total_cells]
 
-    while t < 40:
+    while t < 10:
         vegetation.update()
-        alive.append(vegetation.total_alive())
+        alive.append(vegetation.total_alive() / total_cells)
         t += 1
 
+    print(vegetation.total_alive())
     fig, ax = plot_grid(vegetation)
     fig.savefig("veg_grid_after.png", dpi=300)
 
-    # Plot ratio of alive vs dead cells
-    # vegetation = Vegetation(128)
-    # vegetation.initial_grid(p=0.5)
-    # ani = animate_ca(vegetation, 1)
-    # plt.show()
-    # ani.save("vegetation.mp4")
+    iterations = list(range(len(alive)))
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(iterations, alive, linestyle="-")
+    plt.title("Proportion of Alive Plants vs Iterations")
+    plt.xlabel("Time Step")
+    plt.ylabel("Proportion of Alive Plants")
+    plt.savefig("proportion_alive.png", dpi=300)
