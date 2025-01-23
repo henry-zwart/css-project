13924c3451ea4dcb0e57b49217aa8e8496e4d433
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-01-23 19:32:27 +0100

    update finegrained plots

--- scripts/plot_simple_finegrained.py
+++ scripts/plot_simple_finegrained.py
@@ -3,18 +3,19 @@ from css_project.visualisation import animate_ca, animate_nutrients
 
 if __name__ == "__main__":
     width = 128
+
     model = FineGrained(
         width,
         nutrient_level=1.0,
-        nutrient_diffusion_rate=0.15,
-        nutrient_consume_rate=0.025,
+        nutrient_diffusion_rate=0.21,  # 0.225
+        nutrient_consume_rate=0.1,
+        nutrient_regenerate_rate=0.8,
     )
     model.initial_grid(p=0.1)
-    initial_state = model.grid.copy()
 
-    ani = animate_nutrients(model, 30, fps=2)
+    ani = animate_nutrients(model, 300, fps=5)
     ani.save("nutrient_diffusion.mp4")
 
-    model.grid = initial_state
-    ani = animate_ca(model, 30, fps=2)
+    model.reset()
+    ani = animate_ca(model, 300, fps=5)
     ani.save("fine_grained_vegetation_spread.mp4")
