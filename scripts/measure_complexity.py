d9592c87959a9c0eff986c32605526f2277207d5
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-01-24 12:35:08 +0100

    add kolmogorov complexity estimation

---
+++ scripts/measure_complexity.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+from tqdm import trange
+
+from css_project.complexity import Compression, compressed_size
+from css_project.vegetation import Vegetation
+from css_project.visualisation import plot_grid
+
+
+def main():
+    # Prepare states
+    WIDTH = 64
+    p = 0.5
+    weight = (3, 4, 6, 8, 10, 13, 17)
+    models = [Vegetation(WIDTH, positive_factor=w) for w in weight]
+    for m in models:
+        m.initial_grid(p)
+        for _ in trange(50):
+            m.update()
+
+    complexities = [[] for _ in Compression]
+    for m in models:
+        print(f"Positive weight: {m.positive_factor}")
+        for i, compression in enumerate(Compression):
+            kc = compressed_size(m.grid, compression)
+            complexities[i].append(kc)
+            print(f"KC({compression}) = {kc:.2f}")
+        print()
+
+    fig, ax = plt.subplots(layout="constrained")
+    for i, compression in enumerate(Compression):
+        ax.plot(weight, complexities[i], label=compression)
+        ax.scatter(weight, complexities[i])
+    ax.set_xlabel("Positive weight coefficient")
+    ax.set_ylabel("Compressed size")
+    ax.set_title("KC estimated as compressed size (50 updates)")
+    fig.legend()
+    fig.savefig("complexity.png")
+
+    for m in models:
+        fig, _ = plot_grid(m)
+        fig.suptitle(f"Positive weight: {m.positive_factor}")
+        fig.savefig(f"complexity_{m.positive_factor}.png")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/css_project/complexity.py
@@ -0,0 +1,37 @@
+import bz2
+import gzip
+import lzma
+import pickle
+import zlib
+from enum import StrEnum
+
+import numpy as np
+
+
+class Compression(StrEnum):
+    ZLIB = "zlib"
+    GZIP = "gzip"
+    BZ2 = "bz2"
+    LZMA = "lzma"
+
+    def compress(self, string: bytes):
+        match self:
+            case Compression.ZLIB:
+                return zlib.compress(string)
+            case Compression.GZIP:
+                return gzip.compress(string)
+            case Compression.BZ2:
+                return bz2.compress(string)
+            case Compression.LZMA:
+                return lzma.compress(string)
+
+
+def compressed_size(arr: np.ndarray, compression: Compression) -> int:
+    """Compress a numpy array, and return the compressed size.
+
+    This gives an estimate of the Kolmogorov complexity of the array.
+    Supported compression methods:
+        - zlib
+    """
+    serialised_arr = pickle.dumps(arr)
+    return len(compression.compress(serialised_arr))
