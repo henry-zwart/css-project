60b6cd4aea8e016379d80053968875a1678873e6
lisa1912 <lpijpers03@hotmail.com>
2025-01-24 15:45:30 +0100

    Add visualization for a power law fit

--- scripts/visualise_vegetation.py
+++ scripts/visualise_vegetation.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import powerlaw
 from scipy.ndimage import label, sum
 
 from css_project.vegetation import InvasiveVegetation, Vegetation
@@ -10,8 +11,9 @@ if __name__ == "__main__":
     width = 256
 
     # This code runs without invasive species
-    """vegetation = Vegetation(width)
-    vegetation.initial_grid(p=0.5)
+    vegetation = Vegetation(width)
+    vegetation.initial_grid(p=0.1)
+    vegetation.positive_factor = 8
 
     initial_grid = vegetation.grid.copy()
 
@@ -27,69 +29,13 @@ if __name__ == "__main__":
     total_cells = width * width
     alive = [vegetation.total_alive() / total_cells]
 
-    while t < 20:
+    while t < 50:
         vegetation.update()
         alive.append(vegetation.total_alive() / total_cells)
         t += 1
 
     fig, ax = plot_grid(vegetation)
     fig.savefig("veg_grid_after.png", dpi=300)
-    
-    # Plot ratio of alive vs dead cells
-    # vegetation = Vegetation(128)
-    # vegetation.initial_grid(p=0.5)
-    # ani = animate_ca(vegetation, 1)
-    # plt.show()
-    # ani.save("vegetation.mp4")
-    # """
-
-    # This code runs with invasive species
-
-    vegetation = InvasiveVegetation(width)
-    vegetation.initial_grid()
-
-    # Copy grid to use in graph
-    initial_grid = vegetation.grid.copy()
-
-    fig, ax = plot_grid(vegetation)
-    fig.savefig("veg_grid.png", dpi=300)
-
-    t = 0
-    total_cells = width * width
-
-    alive_nat = []
-    alive_inv = []
-    alive_n, alive_i = vegetation.total_alive()
-    alive_nat.append(alive_n / total_cells)
-    alive_inv.append(alive_i / total_cells)
-
-    while t < timespan:
-        vegetation.update()
-        alive_n, alive_i = vegetation.total_alive()
-        alive_nat.append(alive_n / total_cells)
-        alive_inv.append(alive_i / total_cells)
-        t += 1
-
-    fig, ax = plot_grid(vegetation)
-    fig.savefig("veg_grid_after.png", dpi=300)
-
-    # Reset grid to initial state
-    vegetation.grid = initial_grid.copy()
-
-    # Make animation of grid
-    ani = animate_ca(vegetation, timespan)
-    ani.save("vegetation.gif")
-
-    # Plot ratio of dead, native, and invasive cells
-    iterations = list(range(len(alive_nat)))
-
-    plt.figure(figsize=(8, 6))
-    plt.plot(iterations, alive_nat, linestyle="-", label="Native Species")
-    plt.plot(iterations, alive_inv, linestyle="-", label="Invasive Species")
-    plt.title("Proportion of Native and Invasive species vs Iterations")
-    plt.xlabel("Time Step")
-    plt.ylabel("Proportion Cells")
-    plt.savefig("proportion_nat_inv.png", dpi=300)
 
     # Cluster size distribution
 
@@ -108,7 +54,7 @@ if __name__ == "__main__":
     # Find frequencies of the found cluster sizes
     clusters_freq = np.unique(clusters, return_counts=True)
 
-    # Plot
+    # Plot of cluster size distribution
     plt.figure(figsize=(8, 6))
     plt.plot(clusters_freq[0], clusters_freq[1], "o")
     plt.xlim([6, 1000])
@@ -118,4 +64,72 @@ if __name__ == "__main__":
     plt.xlabel("Cluster size")
     plt.ylabel("Number of patches")
     plt.savefig("cluster_size_distribution.png", dpi=300)
-    plt.show()
+
+    # Use powerlaw package to fit the data
+    results = powerlaw.Fit(clusters)
+
+    # Plot of probability density function and power law
+    plt.figure(figsize=(8, 6))
+    results.plot_pdf(label="Cluster Size Data")
+    results.power_law.plot_pdf(linestyle="--", label="Power Law Fit")
+    plt.legend()
+    plt.xlabel("Cluster Size")
+    plt.ylabel("Probability Density")
+    plt.title("Cluster Size Power Law")
+    plt.savefig("cluster_size_power_law.png", dpi=300)
+    
+    # Plot ratio of alive vs dead cells
+    # vegetation = Vegetation(128)
+    # vegetation.initial_grid(p=0.5)
+    # ani = animate_ca(vegetation, 1)
+    # plt.show()
+    # ani.save("vegetation.mp4")
+    # """
+
+    # This code runs with invasive species
+
+    # vegetation = InvasiveVegetation(width)
+    # vegetation.initial_grid()
+
+    # # Copy grid to use in graph
+    # initial_grid = vegetation.grid.copy()
+
+    # fig, ax = plot_grid(vegetation)
+    # fig.savefig("veg_grid.png", dpi=300)
+
+    # t = 0
+    # total_cells = width * width
+
+    # alive_nat = []
+    # alive_inv = []
+    # alive_n, alive_i = vegetation.total_alive()
+    # alive_nat.append(alive_n / total_cells)
+    # alive_inv.append(alive_i / total_cells)
+
+    # while t < timespan:
+    #     vegetation.update()
+    #     alive_n, alive_i = vegetation.total_alive()
+    #     alive_nat.append(alive_n / total_cells)
+    #     alive_inv.append(alive_i / total_cells)
+    #     t += 1
+
+    # fig, ax = plot_grid(vegetation)
+    # fig.savefig("veg_grid_after.png", dpi=300)
+
+    # # Reset grid to initial state
+    # vegetation.grid = initial_grid.copy()
+
+    # # Make animation of grid
+    # ani = animate_ca(vegetation, timespan)
+    # ani.save("vegetation.gif")
+
+    # # Plot ratio of dead, native, and invasive cells
+    # iterations = list(range(len(alive_nat)))
+
+    # plt.figure(figsize=(8, 6))
+    # plt.plot(iterations, alive_nat, linestyle="-", label="Native Species")
+    # plt.plot(iterations, alive_inv, linestyle="-", label="Invasive Species")
+    # plt.title("Proportion of Native and Invasive species vs Iterations")
+    # plt.xlabel("Time Step")
+    # plt.ylabel("Proportion Cells")
+    # plt.savefig("proportion_nat_inv.png", dpi=300)
