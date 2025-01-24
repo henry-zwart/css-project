0390413b4f35d406b8e822e578963d1ac5e19291
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-01-24 16:51:36 +0100

    shift to inheriting from base class

--- src/css_project/vegetation.py
+++ src/css_project/vegetation.py
@@ -1,13 +1,11 @@
 import numpy as np
 
+from .model import VegetationModel
+
 np.random.seed(2)
 
 
-class Vegetation:
-    N_STATES = 2
-
-    grid: np.ndarray
-
+class Vegetation(VegetationModel):
     def __init__(
         self,
         width: int = 128,
@@ -15,81 +13,18 @@ class Vegetation:
         large_radius: int = 4,
         positive_factor: int = 7,
         negative_factor: int = 1,
+        init_method="random",
+        alive_prop: float = 0.5,
     ):
-        self.grid = np.zeros((width, width), dtype=int)
-        self.width = width
+        super().__init__(width, alive_prop, init_method)
         self.small_radius = small_radius
         self.large_radius = large_radius
         self.positive_factor = positive_factor
         self.negative_factor = negative_factor
-        self.area = width * width
-        self.proportion_alive_list = []
 
-    def initial_grid(self, p):
-        random_matrix = np.random.random(self.grid.shape)
-        self.grid = np.where(random_matrix < p, 1, 0)
-
-    def find_neighbors(self, x, y, radius):
-        """Positive = close
-        Negative = far"""
-        indexes = []
-
-        left = -1 * radius
-        right = radius + 1
-
-        for delta_y in range(left, right):
-            if y + delta_y < 0 or y + delta_y > self.width - 1:
-                continue
-            for delta_x in range(left, right):
-                if x + delta_x < 0 or x + delta_x > self.width - 1:
-                    continue
-                if delta_x == 0 and delta_y == 0:
-                    continue
-                indexes.append([x + delta_x, y + delta_y])
-
-        return indexes
-
-    def find_states(self, neighbors):
-        alive = 0
-
-        # Sums up all 'alive' neighbors
-        for row, column in neighbors:
-            alive += self.grid[row, column]
-
-        return alive
-
-    def total_alive(self):
-        """Counts total number of alive cells in the grid."""
-
-        alive = self.grid.sum()
-
-        return alive
-
-    def is_steady_state(self):
-        """Checks whether a steady state is reached using first order and second
-        order difference"""
-
-        # Selects 21 last iterations to look at a trend instead of local fluctuation
-        # and the index of the middle term of second difference was
-        # a whole integer
-        if (len(self.proportion_alive_list)) > 21:
-            der = self.proportion_alive_list[-21] - self.proportion_alive_list[-1]
-            der_2 = (
-                self.proportion_alive_list[-21]
-                + self.proportion_alive_list[-1]
-                - 2 * self.proportion_alive_list[-11]
-            )
-            if abs(der) < 0.001 and abs(der_2) < 0.001:
-                print(f"difference: {der}, second order difference: {der}")
-                return True
-        return False
-
-    def find_steady_state(self, iterations):
-        for iter in range(iterations):
-            if self.is_steady_state():
-                print(f"Iteration: {iter}")
-                break
-            self.update()
+    @property
+    def n_states(self) -> int:
+        return 2
 
     def update(self):
         temp_grid = np.empty_like(self.grid)
@@ -98,11 +33,11 @@ class Vegetation:
             for x in range(self.width):
                 # Find local neighbors and calculate sum
                 close = self.find_neighbors(y, x, self.small_radius)
-                close_sum = self.find_states(close)
+                close_sum = self.find_states(close)[0]
 
                 # Find non-local neighbors and calculate sum
                 far = self.find_neighbors(y, x, self.large_radius)
-                far_sum = self.find_states(far)
+                far_sum = self.find_states(far)[0]
 
                 # Calculate positive and negative feedback
                 feedback = (
@@ -122,11 +57,7 @@ class Vegetation:
         self.proportion_alive_list.append(self.total_alive() / self.area)
 
 
-class InvasiveVegetation:
-    N_STATES = 3
-
-    grid: np.ndarray
-
+class InvasiveVegetation(VegetationModel):
     def __init__(
         self,
         width: int = 128,
@@ -136,9 +67,10 @@ class InvasiveVegetation:
         neg_factor_nat: int = 1,
         pos_factor_inv: int = 8,
         neg_factor_inv: int = 1,
+        init_method="random",
+        species_prop: list[float] | tuple[float, float] | np.ndarray = (0.25, 0.25),
     ):
-        self.grid = np.zeros((width, width), dtype=int)
-        self.width = width
+        super().__init__(width, list(species_prop), init_method)
         self.small_radius = small_radius
         self.large_radius = large_radius
         self.pos_factor_nat = pos_factor_nat
@@ -146,67 +78,14 @@ class InvasiveVegetation:
         self.pos_factor_inv = pos_factor_inv
         self.neg_factor_inv = neg_factor_inv
 
-    def initial_grid(self, p_nat=0.25, p_inv=0.25, type="random"):
-        """p_nat and p_inv should be percentages for
-        which something occurs. Example:
-        p_nat = 0.3 so 30% of the initial grid is native.
-        The total of p_nat and p_inv cannot larger than 1 (100%).
-        """
-        if (p_nat + p_inv) > 1:
-            raise ValueError("Total of p_nat and p_inv cannot be larger than 1")
-
-        # Set the percentile region for p_inv
-        p_inv += p_nat
-
-        if type == "random":
-            random_matrix = np.random.random(self.grid.shape)
-            self.grid[np.where(random_matrix <= p_nat)] = 1
-            self.grid[np.where((random_matrix > p_nat) & (random_matrix <= p_inv))] = 2
-            self.grid[np.where(random_matrix > p_inv)] = 0
-
-        elif type == "equilibrium":
-            random_matrix = np.random.random(self.grid.shape)
-            self.grid[np.where(random_matrix <= p_nat)] = 1
-            self.grid[np.where(random_matrix > p_nat)] = 0
+    @property
+    def n_states(self) -> int:
+        return 3
 
     def introduce_invasive(self, p_inv=0.1, type="random"):
         if type == "random":
             random_matrix = np.random.random(self.grid.shape)
             self.grid[np.where(random_matrix <= p_inv)] = 2
-
-    def find_neighbors(self, x, y, radius):
-        """Positive = close
-        Negative = far"""
-        indexes = []
-
-        left = -1 * radius
-        right = radius + 1
-
-        for delta_y in range(left, right):
-            if y + delta_y < 0 or y + delta_y > self.width - 1:
-                continue
-            for delta_x in range(left, right):
-                if x + delta_x < 0 or x + delta_x > self.width - 1:
-                    continue
-                if delta_x == 0 and delta_y == 0:
-                    continue
-                indexes.append([x + delta_x, y + delta_y])
-
-        return indexes
-
-    def find_states(self, neighbors):
-        """return number of 1 and 2's"""
-        native = 0
-        invasive = 0
-
-        # Sums up all 'alive' neighbors
-        for row, column in neighbors:
-            if self.grid[row, column] == 1:
-                native += 1
-            elif self.grid[row, column] == 2:
-                invasive += 1
-
-        return native, invasive
 
     def update(self):
         temp_grid = np.empty_like(self.grid)
@@ -216,7 +95,6 @@ class InvasiveVegetation:
                 # Find local neighbors and calculate sum
                 close = self.find_neighbors(y, x, self.small_radius)
                 close_nat, close_inv = self.find_states(close)
-                # close_sum = self.find_states(close)
 
                 # Find non-local neighbors and calculate sum
                 far = self.find_neighbors(y, x, self.large_radius)
@@ -263,12 +141,12 @@ class InvasiveVegetation:
 
         self.grid = temp_grid
 
-    def total_alive(self):
-        """Counts total number of alive cells in the grid."""
-        alive_nat = 0
-        alive_inv = 0
+    # def total_alive(self):
+    #    """Counts total number of alive cells in the grid."""
+    #    alive_nat = 0
+    #    alive_inv = 0
 
-        alive_nat += (self.grid == 1).sum().item()
-        alive_inv += (self.grid == 2).sum().item()
+    #    alive_nat += (self.grid == 1).sum().item()
+    #    alive_inv += (self.grid == 2).sum().item()
 
-        return alive_nat, alive_inv
+    #    return alive_nat, alive_inv
