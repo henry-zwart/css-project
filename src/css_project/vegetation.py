c0254665300ef3a9e63d59809756483e98accc00
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-01-24 22:18:01 +0100

    add docstrings

--- src/css_project/vegetation.py
+++ src/css_project/vegetation.py
@@ -9,6 +9,28 @@ np.random.seed(2)
 
 
 class Vegetation(VegetationModel):
+    """Vegetation cellular automata model for native species.
+
+    Vegetation growth is governed by a neighborhood feedback rule. Positive
+    and negative feedbacks scale linearly with the quantity of vegetation in
+    separate radii. The former reflects the positive contribution of nearby
+    vegetation to soil quality. The latter reflects the competition for local
+    resources (nutrients).
+
+    Attributes:
+        positive_factor: Weight coefficient applied to positive feedback
+        negative_factor: Weight coefficient applied to negative feedback
+        close_kernel: Convolution kernel used to compute the number of nearby
+            neighbors for positive feedback.
+        far_kernel: Convolution kernel used to compute the number of nearby
+            neighbors for negative feedback.
+    """
+
+    positive_factor: int
+    negative_factor: int
+    close_kernel: np.ndarray
+    far_kernel: np.ndarray
+
     def __init__(
         self,
         width: int = 128,
@@ -19,23 +41,58 @@ class Vegetation(VegetationModel):
         init_method="random",
         alive_prop: float = 0.5,
     ):
+        """Initialise the Vegetation cellular automata model.
+
+        Args:
+            width: The number of cells in one side of the cellular automata grid.
+            small_radius: The square radius within which to count neighbors for
+                positive feedback.
+            large_radius: The square radius within which to count neighbors for
+                negative feedback.
+            positive_factor: Weight coefficient applied to positive feedback.
+            negative_factor: Weight coefficient applied to negative feedback.
+            init_method: Method used to initialise population on the grid.
+            alive_prop: Initial proportion of occupied cells.
+        """
         super().__init__(width, alive_prop, init_method)
-        self.small_radius = small_radius
-        self.large_radius = large_radius
         self.positive_factor = positive_factor
         self.negative_factor = negative_factor
-        self.close_kernel = neighbour_count_kernel(self.small_radius)
-        self.far_kernel = neighbour_count_kernel(self.large_radius)
+        self.close_kernel = neighbour_count_kernel(small_radius)
+        self.far_kernel = neighbour_count_kernel(large_radius)
 
     @property
     def n_states(self) -> int:
+        """Number of states in the model."""
         return 2
 
     def compute_feedback(self, n_close: np.ndarray, n_far: np.ndarray) -> np.ndarray:
+        """Calculate feedback as a linear combination of neighbour frequencies.
+
+        The feedback is rounded toward zero, and clipped to be in the range [-1, 1].
+        Values after rounding and clipping are:
+        - -1, if feedback <= -1
+        - 0, if -1 < feedback < 1
+        - 1, if 1 <= feedback
+
+        Args:
+            n_close: For each cell, the number of nearby neighbors.
+            n_far: For each cell, the number of distance neighbors.
+
+        Returns:
+            The feedback value for each cell in the grid.
+        """
         raw_feedback = self.positive_factor * n_close - self.negative_factor * n_far
         return np.clip(np.fix(raw_feedback), -1, 1).astype(int)
 
     def update(self):
+        """Perform a single transition on the grid.
+
+        Calculate the feedback for each cell in the grid.
+        Feedback:
+        - **-1:** Cell dies (or stays dead)
+        - **0**: Cell retains its state
+        - **1**: Cell becomes alive (or stays alive)
+        """
         close_neighbours = count_neighbours(self.grid, self.close_kernel)
         far_neighbours = count_neighbours(self.grid, self.far_kernel)
         feedback = self.compute_feedback(close_neighbours, far_neighbours)
