d46dca8f00f937c25f0d19c031d92c78e2acc632
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-01-24 10:28:06 +0100

    add script to benchmark finegrained model

---
+++ scripts/run_finegrained.py
@@ -0,0 +1,23 @@
+import argparse
+
+from css_project.fine_grained import FineGrained
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="FineGrained", description="Run finegrained model"
+    )
+    parser.add_argument("--width", type=int)
+    args = parser.parse_args()
+
+    model = FineGrained(
+        args.width,
+        nutrient_level=1.0,
+        nutrient_diffusion_rate=0.21,  # 0.225
+        nutrient_consume_rate=0.1,
+        nutrient_regenerate_rate=0.8,
+        random_seed=42,
+    )
+    model.initial_grid(p=0.1)
+
+    for _ in range(100):
+        model.update()
