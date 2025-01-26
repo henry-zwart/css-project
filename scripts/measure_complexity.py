a24a0ff46ba6ece6578dd4feaf355ec0d97f4610
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-01-26 13:15:59 +0100

    add clustering-based complexity measures

--- scripts/measure_complexity.py
+++ scripts/measure_complexity.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
 from tqdm import trange
 
-from css_project.complexity import Compression, compressed_size
+from css_project.complexity import (
+    Compression,
+    compressed_size,
+    count_clusters,
+    maximum_cluster_size,
+    ratio_cluster_size,
+)
 from css_project.vegetation import Vegetation
 from css_project.visualisation import plot_grid
 
@@ -17,14 +23,12 @@ def main():
         for _ in trange(50):
             m.update()
 
+    # Kolmogorov complexity
     complexities = [[] for _ in Compression]
     for m in models:
-        print(f"Positive weight: {m.positive_factor}")
         for i, compression in enumerate(Compression):
             kc = compressed_size(m.grid, compression)
             complexities[i].append(kc)
-            print(f"KC({compression}) = {kc:.2f}")
-        print()
 
     fig, ax = plt.subplots(layout="constrained")
     for i, compression in enumerate(Compression):
@@ -34,8 +38,37 @@ def main():
     ax.set_ylabel("Compressed size")
     ax.set_title("KC estimated as compressed size (50 updates)")
     fig.legend()
-    fig.savefig("complexity.png")
+    fig.savefig("kolmogorov_complexity.png")
 
+    # Cluster size (maximum and ratio max-min)
+    cluster_count = []
+    max_clusters = []
+    ratio_clusters = []
+    for m in models:
+        cluster_count.append(count_clusters(m.grid))
+        max_clusters.append(maximum_cluster_size(m.grid))
+        ratio_clusters.append(ratio_cluster_size(m.grid))
+
+    fig, axes = plt.subplots(
+        nrows=3, ncols=1, sharex=True, figsize=(8, 6), layout="constrained"
+    )
+    axes[0].plot(weight, ratio_clusters)
+    axes[0].scatter(weight, ratio_clusters)
+    axes[0].set_ylim(0, None)
+    axes[1].plot(weight, cluster_count)
+    axes[1].scatter(weight, cluster_count)
+    axes[1].set_ylim(0, None)
+    axes[2].plot(weight, max_clusters)
+    axes[2].scatter(weight, max_clusters)
+    axes[2].set_ylim(0.0, 1.0)
+    axes[0].set_ylabel("Max/min size")
+    axes[1].set_ylabel("Number of clusters")
+    axes[2].set_ylabel("Maximum size")
+    axes[2].set_xlabel("Positive weight coefficient")
+    fig.suptitle("Cluster size complexity for varying +ve weight")
+    fig.savefig("cluster_complexity.png", dpi=500)
+
+    # Plot the grids
     for m in models:
         fig, _ = plot_grid(m)
         fig.suptitle(f"Positive weight: {m.positive_factor}")
--- src/css_project/complexity.py
+++ src/css_project/complexity.py
@@ -6,6 +6,9 @@ import zlib
 from enum import StrEnum
 
 import numpy as np
+from scipy import ndimage
+
+CLUSTER_COUNT_STRUCTURE = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=int)
 
 
 class Compression(StrEnum):
@@ -38,3 +41,86 @@ def compressed_size(arr: np.ndarray, compression: Compression) -> float:
     """
     serialised_arr = pickle.dumps(arr)
     return len(compression.compress(serialised_arr)) / len(serialised_arr)
+
+
+def _get_cluster_sizes(arr: np.ndarray) -> np.ndarray:
+    """Calculate the cluster sizes in a 2D grid.
+
+    Clustering assumes a Moore's neighbourhood, so includes diagonal neighbours.
+    Two neighbouring cells are considered part of a cluster if their values are
+    non-zero.
+
+    Args:
+        arr: A 2D numpy array of integers representing the states of the grid
+            cells.
+
+    Returns:
+        Numpy array containing the sizes of identified clusters.
+    """
+    cluster_matrix, _ = ndimage.label(arr, structure=CLUSTER_COUNT_STRUCTURE)
+    return ndimage.sum(arr, cluster_matrix, index=np.arange(cluster_matrix.max() + 1))
+
+
+def count_clusters(arr: np.ndarray) -> int:
+    """Get the number of clusters in a cellular automata grid.
+
+    Assumes a Moore's neighbourhood, so diagonal neighbours are included.
+
+    Args:
+        arr: A 2D numpy array of integers representing the states of the grid
+            cells.
+
+    Returns:
+        Integer number of clusters identified.
+    """
+    _, n_clusters = ndimage.label(arr, structure=CLUSTER_COUNT_STRUCTURE)
+    return n_clusters
+
+
+def ratio_cluster_size(arr: np.ndarray) -> float:
+    """Ratio of largest to smallest cluster size in a cellular automata grid.
+
+    Assumes a Moore's neighbourhood, so diagonal neighbours are included.
+
+    Args:
+        arr: A 2D numpy array of integers representing the states of the grid
+            cells.
+
+    Returns:
+        The ratio of maximum to minimum cluster sizes (maximum / minimum).
+        `np.inf` returned if no clusters are found.
+
+    Raises:
+        ValueError: If supplied array is not 2D.
+    """
+    if arr.ndim != 2:
+        raise ValueError(f"Expected 2D array, received array of shape: {arr.shape}")
+
+    cluster_sizes = _get_cluster_sizes(arr)[1:]
+    try:
+        return cluster_sizes.max() / cluster_sizes.min()
+    except ValueError:
+        return np.inf
+    except:
+        raise
+
+
+def maximum_cluster_size(arr: np.ndarray) -> float:
+    """Proportional size of the maximum cluster in a cellular automata grid.
+
+    Assumes a Moore's neighbourhood, so diagonal neighbours are included.
+
+    Args:
+        arr: A 2D numpy array of integers representing the states of the grid
+            cells.
+
+    Returns:
+        The maximum cluster size as a proportion of the number of grid cells.
+
+    Raises:
+        ValueError: If supplied array is not 2D.
+    """
+    if arr.ndim != 2:
+        raise ValueError(f"Expected 2D array, received array of shape: {arr.shape}")
+    cluster_sizes = _get_cluster_sizes(arr)
+    return cluster_sizes.max() / (arr.shape[0] * arr.shape[1])
