12bf142ba60e91f79ec26c0ce60f8f6064ae890d
lisa1912 <lpijpers03@hotmail.com>
2025-01-27 15:10:29 +0100

    Made functions for the visualizations and added feature to vary positive weight

--- scripts/proportion_alive_pos_weight.py
+++ scripts/proportion_alive_pos_weight.py
@@ -2,27 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from css_project.vegetation import Vegetation
+from css_project.visualisation import phase_transition_pos_weight
 
-# from css_project.visualisation import animate_ca, plot_grid
 pos_weight_list = np.linspace(0, 20, 21)
-
 width = 64
 
-iterations_list = []
-alive_list = []
-
-for pos_weight in pos_weight_list:
-    vegetation = Vegetation(width, alive_prop=0.5)
-    vegetation.positive_factor = pos_weight
-    Vegetation.find_steady_state(vegetation, 1000)
-    alive_list.append(vegetation.proportion_alive_list)
-
-plt.figure(figsize=(8, 6))
-
-steady_alive_list = [x[-1] for x in alive_list]
-plt.scatter(pos_weight_list, steady_alive_list)
-
-plt.title("Proportion of Alive Cells vs Positive Weight")
-plt.xlabel("Positive Weight")
-plt.ylabel("Proportion of Alive Cells")
-plt.savefig("proportion_alive_on_pos_weight", dpi=300)
+fig = phase_transition_pos_weight(width, pos_weight_list)
+fig.savefig("proportion_alive_on_pos_weight", dpi=300)
--- scripts/proportion_alive_starting_prob.py
+++ scripts/proportion_alive_starting_prob.py
@@ -1,65 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.pyplot import cm
-from tqdm import tqdm
 
 from css_project.vegetation import Vegetation
-
-# from css_project.visualisation import animate_ca, plot_grid
-p_list = [
-    0.001,
-    0.0015,
-    0.0018,
-    0.002,
-    0.003,
-    0.004,
-    0.005,
-    0.006,
-    0.007,
-    0.008,
-    0.009,
-    0.01,
-    0.1,
-]
+from css_project.visualisation import phase_transition_prob
 
 p_list = np.logspace(start=-4, stop=0, num=100, base=10)
 width = 64
+pos_weight_list = [5, 7, 9]
 
-iterations_list = []
-alive_list = []
+fig1, fig2 = phase_transition_prob(width, p_list, pos_weight_list)
 
-for p in tqdm(p_list):
-    vegetation = Vegetation(width, alive_prop=p)
-    vegetation.find_steady_state(1000)
-    iterations = list(range(len(vegetation.proportion_alive_list)))
-    alive_list.append(vegetation.proportion_alive_list)
-    iterations_list.append(iterations)
-
-
-plt.figure(figsize=(8, 6))
-
-num_of_lines = len(p_list)
-color = iter(cm.cool(np.linspace(0, 1, num_of_lines)))
-
-for i in range(len(p_list)):
-    c = next(color)
-    plt.plot(
-        iterations_list[i], alive_list[i], c=c, linestyle="-", label=f"{p_list[i]}"
-    )
-
-
-plt.title("Proportion of Alive Cells vs Iterations")
-plt.xlabel("Time Step")
-plt.ylabel("Proportion of Alive Cells")
-plt.savefig("proportion_alive_over_iter", dpi=300)
-
-plt.figure(figsize=(8, 6))
-
-steady_alive_list = [x[-1] for x in alive_list]
-plt.scatter(p_list, steady_alive_list)
-
-plt.xscale("log")
-plt.title("Proportion of Alive Cells vs Iterations")
-plt.xlabel("Time Step")
-plt.ylabel("Proportion of Alive Cells")
-plt.savefig("proportion_alive_on_probability", dpi=300)
+fig1.savefig("proportion_alive_over_iter", dpi=300)
+fig2.savefig("proportion_alive_on_probability", dpi=300)
