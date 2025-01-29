44d4f0359b68f0d3427fc645efeaad25df4868af
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-01-29 22:09:44 +0100

    add plot of clusters

--- scripts/measure_complexity.py
+++ scripts/measure_complexity.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
-from tqdm import trange
+import numpy as np
+from scipy.ndimage import label
 
 from css_project.complexity import (
     Compression,
     compressed_size,
     count_clusters,
+    fluctuation_cluster_size,
     maximum_cluster_size,
-    ratio_cluster_size,
 )
 from css_project.vegetation import Vegetation
 from css_project.visualisation import plot_grid
@@ -14,15 +15,15 @@ from css_project.visualisation import plot_grid
 
 def main():
     # Prepare states
-    WIDTH = 64
-    p = 0.5
+    WIDTH = 128
+    p = 0.01
 
-    weight = (3, 4, 6, 8, 9, 10, 11, 13, 17)
-    models = [Vegetation(WIDTH, positive_factor=w, alive_prop=p) for w in weight]
+    weight = (3, 4, 6, 8, 9, 10, 11, 12, 13)
+    # weight = (33.5, 33.7, 33.9, 34.1, 34.3, 34.5, 34.7, 34.9, 35.1)
+    models = [Vegetation(WIDTH, control=w, alive_prop=p) for w in weight]
 
     for m in models:
-        for _ in trange(50):
-            m.update()
+        m.run(1000)
 
     # Kolmogorov complexity
     complexities = [[] for _ in Compression]
@@ -44,37 +45,50 @@ def main():
     # Cluster size (maximum and ratio max-min)
     cluster_count = []
     max_clusters = []
-    ratio_clusters = []
+    fluctuation_clusters = []
     for m in models:
-        cluster_count.append(count_clusters(m.grid))
+        cluster_count.append(count_clusters(m.grid) / m.area)
         max_clusters.append(maximum_cluster_size(m.grid))
-        ratio_clusters.append(ratio_cluster_size(m.grid))
+        fluctuation_clusters.append(fluctuation_cluster_size(m.grid))
+
+    fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(8, 8), layout="constrained")
+    for ax, model in zip(axes.flatten(), models, strict=True):
+        label_matrix, _ = label(model.grid, structure=np.ones((3, 3)))
+        ax.imshow(label_matrix)
+        ax.set_title(f"{model.positive_factor}")
+    fig.savefig(f"islands_{WIDTH}_vegetation.png", dpi=500)
 
     fig, axes = plt.subplots(
         nrows=3, ncols=1, sharex=True, figsize=(8, 6), layout="constrained"
     )
-    axes[0].plot(weight, ratio_clusters)
-    axes[0].scatter(weight, ratio_clusters)
-    axes[0].set_ylim(0, None)
+    axes[0].plot(weight, fluctuation_clusters)
+    axes[0].scatter(weight, fluctuation_clusters)
+    # axes[0].set_ylim(0, None)
+    axes[0].set_yscale("log")
     axes[1].plot(weight, cluster_count)
     axes[1].scatter(weight, cluster_count)
-    axes[1].set_ylim(0, None)
+    # axes[1].set_ylim(0, None)
+    axes[1].set_yscale("log")
     axes[2].plot(weight, max_clusters)
     axes[2].scatter(weight, max_clusters)
-    axes[2].set_ylim(0.0, 1.0)
-    axes[0].set_ylabel("Max/min size")
+    axes[2].axhline(1, linestyle="dashed", linewidth=1)
+    axes[2].axhline(2, linestyle="dashed", linewidth=1)
+    # axes[2].set_ylim(0.0, 1.0)
+    axes[2].set_yscale("log")
+    axes[0].set_ylabel("Cluster size fluctuation")
     axes[1].set_ylabel("Number of clusters")
     axes[2].set_ylabel("Maximum size")
     axes[2].set_xlabel("Positive weight coefficient")
     fig.suptitle("Cluster size complexity for varying +ve weight")
-    fig.savefig("cluster_complexity.png", dpi=500)
+    fig.savefig(f"cluster_complexity_{WIDTH}_vegetation.png", dpi=500)
 
     # Plot the grids
     fig, axes = plt.subplots(3, 3, figsize=(8, 8), layout="constrained")
     for m, ax in zip(models, axes.flatten(), strict=True):
         _ = plot_grid(m, ax=ax)
-        ax.set_title(f"Positive weight: {m.positive_factor}")
-    fig.savefig("complexity_states_example.png", dpi=500)
+        ax.set_title(f"Nutrient availability: {m.positive_factor}")
+        # ax.set_title(f"Positive weight: {m.positive_factor}")
+    fig.savefig(f"complexity_states_example_{WIDTH}_vegetation.png", dpi=500)
 
 
 if __name__ == "__main__":
