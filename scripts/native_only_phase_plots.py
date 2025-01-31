85e91f03c9c732a9f37625db9c5ea36ba56000c5
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-01-31 14:21:00 +0100

    add 'quick' configurations via cli

--- scripts/native_only_phase_plots.py
+++ scripts/native_only_phase_plots.py
@@ -2,22 +2,25 @@
 Make phase plots for both coarse-grained and logistic model for multiple control
 parameters and initial values.
 """
+
+import argparse
+
 from css_project.logistic import Logistic
 from css_project.vegetation import Vegetation
 from css_project.visualisation import logistic_phase_plot
 
 
-def main():
+def main(widths: list[int], n_repeats: int):
     fig, _ = logistic_phase_plot(
         Vegetation,
-        [64, 128, 256],
-        0.1,
+        widths,
+        0.01,
         [5, 15],
         min_nutrient=1,
         max_nutrient=20,
         n_steps=50,
         iters_per_step=200,
-        n_repeats=20,
+        n_repeats=n_repeats,
         control_variable_name="Positive weight",
     )
 
@@ -25,15 +28,14 @@ def main():
 
     fig, _ = logistic_phase_plot(
         Logistic,
-        [64, 128, 256],
-        0.1,
-        # [6.0, 26.0, 55.0, 65.0],
+        widths,
+        0.01,
         [15.0, 45.0],
         min_nutrient=1,
         max_nutrient=65,
         n_steps=65,
         iters_per_step=200,
-        n_repeats=20,
+        n_repeats=n_repeats,
         control_variable_name="Nutrient availability",
     )
 
@@ -41,4 +43,18 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    QUICK_WIDTHS = [32, 64, 128]
+    QUICK_REPEATS = 2
+    FULL_WIDTHS = [64, 128, 256]
+    FULL_REPEATS = 20
+
+    parser = argparse.ArgumentParser(
+        prog="PhaseTransitions", description="Create continuous phase transition plots"
+    )
+    parser.add_argument("--quick", action="store_true")
+    args = parser.parse_args()
+
+    if args.quick:
+        main(widths=QUICK_WIDTHS, n_repeats=QUICK_REPEATS)
+    else:
+        main(widths=FULL_WIDTHS, n_repeats=FULL_REPEATS)
--- scripts/plot_simple_finegrained.py
+++ scripts/plot_simple_finegrained.py
@@ -1,9 +1,10 @@
+import argparse
+
 from css_project.fine_grained import FineGrained
 from css_project.visualisation import animate_ca, animate_nutrients
 
-if __name__ == "__main__":
-    width = 256
 
+def main(width: int, frames: int):
     model = FineGrained(
         width,
         nutrient_level=1.0,
@@ -13,9 +14,28 @@ if __name__ == "__main__":
     )
     model.initial_grid(p=0.1)
 
-    ani = animate_nutrients(model, 150, fps=10)
+    ani = animate_nutrients(model, frames, fps=10)
     ani.save("results/nutrient_diffusion.gif")
 
     model.reset()
-    ani = animate_ca(model, 150, fps=10)
+    ani = animate_ca(model, frames, fps=10)
     ani.save("results/fine_grained_vegetation_spread.gif")
+
+
+if __name__ == "__main__":
+    QUICK_WIDTH = 64
+    QUICK_FRAMES = 150
+    FULL_WIDTH = 256
+    FULL_FRAMES = 150
+
+    parser = argparse.ArgumentParser(
+        prog="FineGrainedAnimation",
+        description="Create animations for finegrained model",
+    )
+    parser.add_argument("--quick", action="store_true")
+    args = parser.parse_args()
+
+    if args.quick:
+        main(width=QUICK_WIDTH, frames=QUICK_FRAMES)
+    else:
+        main(width=FULL_WIDTH, frames=FULL_FRAMES)
