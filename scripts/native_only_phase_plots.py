9b093ee598a5096f984aeb0a7d13fea156ad7b38
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-01-31 13:45:33 +0100

    set parameters for final plots

--- scripts/native_only_phase_plots.py
+++ scripts/native_only_phase_plots.py
@@ -6,28 +6,31 @@ from css_project.visualisation import logistic_phase_plot
 def main():
     fig, _ = logistic_phase_plot(
         Vegetation,
-        128,
-        0.01,
-        [6, 10, 15],
-        min_nutrient=5,
+        [64, 128, 256],
+        0.1,
+        [5, 15],
+        min_nutrient=1,
         max_nutrient=20,
-        n_steps=20,
+        n_steps=50,
         iters_per_step=200,
-        n_repeats=10,
+        n_repeats=20,
+        control_variable_name="Positive weight",
     )
 
     fig.savefig("results/native_vegetation_phaseplot.png", dpi=300)
 
     fig, _ = logistic_phase_plot(
         Logistic,
-        128,
-        0.01,
-        [6.0, 26.0, 55.0, 65.0],
+        [64, 128, 256],
+        0.1,
+        # [6.0, 26.0, 55.0, 65.0],
+        [15.0, 45.0],
         min_nutrient=1,
-        max_nutrient=80,
-        n_steps=80,
+        max_nutrient=65,
+        n_steps=65,
         iters_per_step=200,
-        n_repeats=10,
+        n_repeats=20,
+        control_variable_name="Nutrient availability",
     )
 
     fig.savefig("results/native_logistic_phaseplot.png", dpi=300)
