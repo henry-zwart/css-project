922edf315a0e0b85d5c387a8d9366af0daee7adb
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-01-31 15:03:34 +0100

    add 'quick' configurations via cli

--- scripts/species_densities_coarsegrained.py
+++ scripts/species_densities_coarsegrained.py
@@ -1,12 +1,33 @@
 """
 Visualise species densities over time for coarse-grained model.
 """
+
+import argparse
+
 from css_project.visualisation import densities_invasive_coarsegrained
 
-width = 128
-p_nat = 0.25
-p_inv = 0.6
 
-fig1 = densities_invasive_coarsegrained(width, p_nat, p_inv)
+def main(width: int):
+    p_nat = 0.25
+    p_inv = 0.6
 
-fig1.savefig("results/species_densities_coarsegrained", dpi=300)
+    fig1 = densities_invasive_coarsegrained(width, p_nat, p_inv)
+
+    fig1.savefig("results/species_densities_coarsegrained", dpi=300)
+
+
+if __name__ == "__main__":
+    QUICK_WIDTH = 128
+    FULL_WIDTH = 256
+
+    parser = argparse.ArgumentParser(
+        prog="SpeciesDensityEvolution",
+        description="Plot evolution of species densities after invasive introduction",
+    )
+    parser.add_argument("--quick", action="store_true")
+    args = parser.parse_args()
+
+    if args.quick:
+        main(width=QUICK_WIDTH)
+    else:
+        main(width=FULL_WIDTH)
